9a1c7862b8759411adcd44c0136173e1375b3927
VictoriaMcCurrey <vdmccurrey@mail.fhsu.edu>
2021-09-20 14:18:32 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let myNumber=5; //question 1 and 2
+console.log(myNumber)
+
+let myOtherNumber=5; //question 3
+console.log(myOtherNumber)
+
+let myTotal=myNumber*myOtherNumber; //question 4
+console.log(myTotal)
+
+let myName='Victoria McCurrey', 
+    myCombo=`\nThe sum of 5 + 5 equals ${myNumber+myOtherNumber}`; //question 5
+console.log(myName,myCombo)
+
+let myOtherCombo=myNumber+myOtherNumber; //question 6
+console.log(myOtherCombo)
+
+let myRemainder=myNumber%5; //question 7
+console.log(myRemainder)
+
+let i=2; //question 8
+console.log(i)
+
+let int=4; // question 9
+console.log(int)
+
+let myBoolean=false; //question 10
+console.log(myBoolean)
